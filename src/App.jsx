44ecb9307748c3c56d8a13d939e7b569b2dca040
SakibAlIslam <sakibalislam12345@gmail.com>
2024-05-02 04:12:05 +0600

    Component and squares state created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-export default function App() {
+import { useState } from "react";
+import Square from "./components/Square";
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = () => {
+    
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">tic tac toe</h1>
+    <>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
   )
 }
---
+++ src/components/Square.jsx
@@ -0,0 +1,6 @@
+const Square = ({value, onSquareClick}) => {
+    return (
+        <button onClick={onSquareClick} className="bg-white border border-gray-300 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+    );
+};
+export default Square;
